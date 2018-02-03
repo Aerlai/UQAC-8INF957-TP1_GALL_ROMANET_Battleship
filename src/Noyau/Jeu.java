c3fd1b95e50d2131e19cccb899b32c19b2c9c225
Antoine GALL (Aerlai) <antoine.gall1@uqac.ca>
2018-02-03 10:23:06 -0500

    BugFix

--- src/Noyau/Jeu.java
+++ src/Noyau/Jeu.java
@@ -7,8 +7,8 @@ public class Jeu {
 
     public static void main(String[] args){
         // Création de 2 joueurs
-        Joueur joueurA = new Joueur();
-        Joueur joueurB = new Joueur();
+        Joueur joueurA = new ImplementationJoueur();
+        Joueur joueurB = new ImplementationJoueur();
 
         InterfaceUtilisateur.run();
 
